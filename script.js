e087a52011f08f6c986c69b0b10c509484d42288
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-05-20 22:07:36 +0530

    feat : added new elements

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var stockQuantity = document.querySelector('#quantity-input');
 var currentPriceInput = document.querySelector('#current-price-input');
 var checkButton = document.querySelector('#check-button');
 var output = document.querySelector('#output-area');
+var profitOutput = document.querySelector('#profit-output');
+var lossOutput = document.querySelector('#loss-output');
+var lowerSection = document.querySelector('#lower-section')
 
 
 function getAllValues() {
@@ -23,18 +26,23 @@ function calculateFunction() {
         var profit = (currentInvestmentAmount - initialInvestmentAmount)
         var profitPercentage = ((profit/initialInvestmentAmount)*100)
         output.innerText =  "your profit is " + profit + " and your profit percentage is " + profitPercentage+"%";
-        output.style.color = "green";
+        profitOutput.style.color = "green";
+        lossOutput.style.color = "black";
     }
 
     if (initialInvestmentAmount > currentInvestmentAmount) {
         var loss = (initialInvestmentAmount - currentInvestmentAmount)
         var lossPercentage = ((loss/initialInvestmentAmount)*100)
         output.innerText = "your loss is " + loss + " and your loss percentage is " + lossPercentage+"%";
-        output.style.color = "red";
+        lossOutput.style.color = "red";
+        profitOutput.style.color = "black";
     }
 
     if (initialInvestmentAmount === currentInvestmentAmount) {
         output.innerText = "You have made neither loss nor profit. You are at a break-even point now."
+        lossOutput.style.display = "none";
+        profitOutput.style.display = "none";
+        lowerSection.style.display= "none";
     }
 }
 
