a8b35418a60356e85c60899a1663fdd64dc0556f
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-05-20 23:00:09 +0530

    fix : changed if conditions

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ var checkButton = document.querySelector('#check-button');
 var output = document.querySelector('#output-area');
 var profitOutput = document.querySelector('#profit-output');
 var lossOutput = document.querySelector('#loss-output');
-var lowerSection = document.querySelector('#lower-section')
 
 
 function getAllValues() {
@@ -26,23 +25,22 @@ function calculateFunction() {
         var profit = (currentInvestmentAmount - initialInvestmentAmount)
         var profitPercentage = ((profit/initialInvestmentAmount)*100)
         output.innerText =  "your profit is " + profit + " and your profit percentage is " + profitPercentage+"%";
-        profitOutput.style.color = "green";
-        lossOutput.style.color = "black";
+        profitOutput.style.backgroundColor = "green";
+        lossOutput.style.backgroundColor = "black";
     }
 
     if (initialInvestmentAmount > currentInvestmentAmount) {
         var loss = (initialInvestmentAmount - currentInvestmentAmount)
         var lossPercentage = ((loss/initialInvestmentAmount)*100)
         output.innerText = "your loss is " + loss + " and your loss percentage is " + lossPercentage+"%";
-        lossOutput.style.color = "red";
-        profitOutput.style.color = "black";
+        lossOutput.style.backgroundColor = "red";
+        profitOutput.style.backgroundColor = "black";
     }
 
     if (initialInvestmentAmount === currentInvestmentAmount) {
-        output.innerText = "You have made neither loss nor profit. You are at a break-even point now."
-        lossOutput.style.display = "none";
-        profitOutput.style.display = "none";
-        lowerSection.style.display= "none";
+        output.innerText = "You have made neither loss nor profit. You are at a break-even point now.";
+        lossOutput.style.backgroundColor = "black";
+        profitOutput.style.backgroundColor = "black";
     }
 }
 
